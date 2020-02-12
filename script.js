09bdfd187f68b6d91264164dc61eaa20e8e16b25
Jennifer Mitzman <jenmitz@gmail.com>
2020-02-11 23:49:11 -0800

    correct date now displayed.

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // my API key:  2c6b4fcf87fab7effe688f8694416801
 
+// preset cities
+var cities = ["Portland", "Kansas City", "Seattle", "Austin", "Orlando", "Chicago"]
 
-// display using imperial system instead of kelvin (&units=imperial)
-// city input
-// preset cities for the user to click
-// save previous searches
-// main info i need: city, temp, humidity, date, weather icon, wind speed, uv index
+// display date in today's weather [working]
+var display = moment().format("dddd, MMMM Do");
+var date = $(".dateToday");
+$(date).html(display);
+
+// today: display date, icon, temp, humidity, wind speed, UV index [not working]
+// 5 day forecast: date, icon, temp, humidity [not working]
+// display previous searches [not working]
+
+
+
+
+/////// WIP / attempts /////////
+/* 
 
 function weatherInfo() {
-    var city = $(this).attr("")
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=2c6b4fcf87fab7effe688f8694416801"
-
+    var city = $(this).attr("data");
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=2c6b4fcf87fab7effe688f8694416801";
 
     $.ajax({
         url: queryURL,
@@ -20,13 +30,17 @@ function weatherInfo() {
         var location = "api.openweathermap.org/data/2.5/uvi?" + "2c6b4fcf87fab7effe688f8694416801" + response.city.coord.lat + "&lon=" + response.city.coord.lon;
         console.log(response);  
 
-        var result = $("#city");
-        var name = response.city.name;
+        var result = response.city.name;
         var cityName = $(result).text(name);
         result.append(cityName);
+        console.log(location);
     });
-
-    var displayDate = moment().format("dddd, MMMM Do YYYY");
-    var date = $(".date");
-    $(date).html(display);
-};
+    
+    $("#searchBtn").on("click", function (event) {
+        event.preventDefault();
+    
+        var city = $(".form-control").val().trim();
+        cities.push(city);
+    });
+}; 
+*/
