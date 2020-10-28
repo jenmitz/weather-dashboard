22a3d408c16587b079c9e5fffbcbb62153c927e8
Jennifer Mitzman <jenmitz@gmail.com>
2020-10-27 18:16:49 -0700

    formatted and adjusted things correctly. styling now complete

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
-// my API key:  2c6b4fcf87fab7effe688f8694416801
+// API key:  2c6b4fcf87fab7effe688f8694416801
 
-// preset cities
-var cities = ["Portland", "Kansas City", "Seattle", "Austin", "Orlando", "Chicago"]
+// preset major cities
+let cities = ["Portland", "Kansas City", "Seattle", "Austin", "Orlando", "Chicago"]
 
 // display date in today's weather [working]
-var display = moment().format("dddd, MMMM Do");
-var date = $(".dateToday");
+let display = moment().format("dddd, MMMM Do");
+let date = $(".dateToday");
 $(date).html(display);
 
-// today: display date, icon, temp, humidity, wind speed, UV index [not working]
-// 5 day forecast: date, icon, temp, humidity [not working]
-// display previous searches [not working]
+// current conditions: display date, icon, temp, humidity, wind speed, UV index [not working]
 
+// UV index gives a color based on air quality (favorable, moderate, severe) [not working]
+
+// 5 day forecast when city is viewed: date, icon, temp, humidity [not working]
+
+// display search history [not working]
+
+// city in search history is clicked, presented current and future conditions [not working]
+
+// open weather dashbard, present with las searched city forecast [not working]
 
 
 
 /////// WIP / attempts /////////
-/* 
 
 function weatherInfo() {
-    var city = $(this).attr("data");
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=2c6b4fcf87fab7effe688f8694416801";
+    let city = $(this).attr("data");
+    let queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=2c6b4fcf87fab7effe688f8694416801";
 
     $.ajax({
         url: queryURL,
         method: "GET"
-
     }).then (function(response) {
-        var location = "api.openweathermap.org/data/2.5/uvi?" + "2c6b4fcf87fab7effe688f8694416801" + response.city.coord.lat + "&lon=" + response.city.coord.lon;
+        let location = "api.openweathermap.org/data/2.5/uvi?" + "2c6b4fcf87fab7effe688f8694416801" + response.city.coord.lat + "&lon=" + response.city.coord.lon;
         console.log(response);  
 
-        var result = response.city.name;
-        var cityName = $(result).text(name);
+        let result = response.city.name;
+        let cityName = $(result).text(name);
         result.append(cityName);
         console.log(location);
     });
     
-    $("#searchBtn").on("click", function (event) {
+    $("#searchBtn").on("click", function(event) {
         event.preventDefault();
     
-        var city = $(".form-control").val().trim();
+        let city = $(".form-control").val().trim(); // form-control = search
         cities.push(city);
     });
 }; 
-*/
