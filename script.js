d1e5dcf3f67d8cf939b96f2128c5a68a92996c97
Jennifer Mitzman <jenmitz@gmail.com>
2020-10-28 17:36:57 -0700

    added notes, declared variables

--- script.js
+++ script.js
@@ -1,50 +1,76 @@
-// API key:  2c6b4fcf87fab7effe688f8694416801
+// INCOMPLETE FEATURES //
+// current conditions: display date, icon, temp, humidity, wind speed, UV index
+// UV index gives a color based on air quality (favorable, moderate, severe)
+// 5 day forecast when city is viewed: date, icon, temp, humidity
+// display search history
+// city in search history is clicked, presented current and future conditions
+// open weather dashbard, presented with last searched city forecast
 
-// preset major cities
-let cities = ["Portland", "Kansas City", "Seattle", "Austin", "Orlando", "Chicago"]
+// --------------------- //
 
+// weather condition list.weather.main
+// city name: city.name
+// temperature: list.main.temp (use imperial unit)
+// humidity: list.main.humidity
+// wind speed: list.wind.speed (use imperial unit)
+// UV index: 
+// condition icon: list.weather.icon
+
+// forecast.windSpeed
+// forecast.temperature.value
+// forecast.humidity.unit
+
+// --------------------- //
+
+// API key
+let apiKey = "2c6b4fcf87fab7effe688f8694416801";
+
+// convert into imperial units (farhenheit)
+let units = "&units=imperial";
+
+// input value (searched city)
+let city = $(".input");
+
+// forecast for searched city
+let queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + units + "&appid=" + apiKey;
+
+// presets
+let cities = ["Portland", "Kansas City", "Seattle", "Austin", "Orlando", "Chicago"];
+
+// search for city
+let citySearch = $(".form-control").val().trim(); // form-control = search
+
+console.log(queryURL)
 // display date in today's weather [working]
 let display = moment().format("dddd, MMMM Do");
 let date = $(".dateToday");
 $(date).html(display);
 
-// current conditions: display date, icon, temp, humidity, wind speed, UV index [not working]
-
-// UV index gives a color based on air quality (favorable, moderate, severe) [not working]
-
-// 5 day forecast when city is viewed: date, icon, temp, humidity [not working]
-
-// display search history [not working]
-
-// city in search history is clicked, presented current and future conditions [not working]
-
-// open weather dashbard, present with las searched city forecast [not working]
 
 
-
-/////// WIP / attempts /////////
-
+/////// WIP / Experimentation /////////
 function weatherInfo() {
-    let city = $(this).attr("data");
-    let queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=2c6b4fcf87fab7effe688f8694416801";
-
+    // let city = $(this).attr("data");
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then (function(response) {
-        let location = "api.openweathermap.org/data/2.5/uvi?" + "2c6b4fcf87fab7effe688f8694416801" + response.city.coord.lat + "&lon=" + response.city.coord.lon;
-        console.log(response);  
-
+        // let location = "api.openweathermap.org/data/2.5/uvi?" + "2c6b4fcf87fab7effe688f8694416801" + 
+        //     response.city.coord.lat + "&lon=" + response.city.coord.lon;
+        // console.log(location);  
         let result = response.city.name;
         let cityName = $(result).text(name);
         result.append(cityName);
         console.log(location);
-    });
-    
-    $("#searchBtn").on("click", function(event) {
-        event.preventDefault();
-    
-        let city = $(".form-control").val().trim(); // form-control = search
-        cities.push(city);
-    });
+
+        $("#searchBtn").on("click", function(event) {
+            event.preventDefault();
+            cities.push(citySearch);
+        });
+    });  
 }; 
+
+// register button click
+$(".btn").on("click", function() {
+    console.log($(".btn") + "clicked")
+}); 
