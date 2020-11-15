19867bb056ae679c2ea83799c0d1f9c6459d3c10
Jennifer Mitzman <jenmitz@gmail.com>
2020-11-14 19:37:55 -0800

    added capitalization og each word in input

--- script.js
+++ script.js
@@ -13,12 +13,10 @@
 
 
 // search for city
-let city = document.getElementById("form").textContent; // form-control = search bar
+let city = document.getElementById("form").textContent;
 
-
-  // display current date & time, & update time every second
+// display current date & time, & update time every second
 function updateTime() {
-    //document.getElementById("currentCon").innerHTML = 
     document.getElementById("currentCon").innerHTML = (moment().format("dddd" + ", " + "MMMM Do YYYY" + ", " + "h:mm a"));
 };
 setInterval(updateTime, 1000);
@@ -26,7 +24,7 @@ setInterval(updateTime, 1000);
 updateTime();
 
 
-
+// search for a city function
 function searchWeather (city) {
 
     let apiKey = "f674676e9306d3ea9c5d2d7245918d40";
@@ -36,12 +34,16 @@ function searchWeather (city) {
         url: currentQueryURL,
         method: "GET"
     }).then (function(response) {
+        // get user input
         document.getElementById("city").textContent = city;
-        document.getElementById("tempToday").textContent = "Temperature: " + Math.round(response.main.temp) + "\u00B0" + " f";
-        document.getElementById("windToday").textContent = "Wind Speed: " + Math.round(response.wind.speed) + "mph";
+        // capitilze each word in the input
+        document.getElementById("city").style.textTransform = "capitalize";
+        document.getElementById("tempToday").textContent = "Temperature: " + Math.round(response.main.temp) + "\u00B0" + " F";
+        document.getElementById("windToday").textContent = "Wind Speed: " + Math.round(response.wind.speed) + " mph";
         document.getElementById("humidityToday").textContent = "Humidity: " + response.main.humidity + "%";
     });
 };
+
 
 document.getElementById("searchBtn").addEventListener("click", function (event) {
     event.preventDefault();
@@ -50,9 +52,10 @@ document.getElementById("searchBtn").addEventListener("click", function (event) 
     searchWeather(city);
 });
 
-
+// select all elements with class of presetBtn - these are the preset cities
 let presetBtns = document.querySelectorAll(".presetBtn");
 
+// on every presetBtn element: get text content of button (preset cities) then perform ajax call
 presetBtns.forEach(element => {
     element.addEventListener("click", function () {
         let cityText = this.textContent;
